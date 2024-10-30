fcc60b88d496c46d52ec8de0f0a0c6d13cfd6d11
Neamen Dobamo <68127279+NeamenD@users.noreply.github.com>
2024-10-30 14:11:11 -0600

    added initial data and iterate over it

---
+++ src/components/Data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    titletitle: "Terminal Website",
+    imgUrl: "",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "http://links.com",
+  },
+  {
+    titletitle: "Developer Portfolio",
+    imgUrl: "",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "http://links.com",
+  },
+  {
+    titletitle: "AI Nerf Gun",
+    imgUrl: "",
+    stack: ["HTML", "CSS", "JavaScript"],
+    link: "http://links.com",
+  },
+];
